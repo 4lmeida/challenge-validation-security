bb00d15e7c4cb19286000498c327ac85ade1fa6e
Luis Almeida <lferreira646@gmail.com>
2023-02-16 20:18:19 -0300

    Service, repository, controller Event and city repository

---
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -0,0 +1,35 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        dto = service.insert(dto);
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> pagedAll(Pageable pageable) {
+        Page<EventDTO> page = service.pagedAll(pageable);
+        return ResponseEntity.ok().body(page);
+    }
+
+}
